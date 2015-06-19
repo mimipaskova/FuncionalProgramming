3c3c322ba5a784067b4760f0fe92759f6d506384
mimipaskova <maria.paskova3@gmail.com>
2015-06-19 21:21:57 +0300

    Add some tasks from second lesson

--- week1/first.hs
+++ week1/first.hs
@@ -35,3 +35,38 @@ calculate sign x y = if sign == '+'
 
 -- convert first second = if first == 'usd'
 -- 						then if second == 'bgn'
+
+
+
+-- isValidID id = (id.lenght == 10) && sum(id)%11 = 0
+
+-- 30  Concatenate the lists
+concatenate :: [a] -> [a] -> [a]
+concatenate [] second = second
+concatenate first [] = first
+concatenate (h:t) second = h : concatenate t second
+
+
+-- 31Take all elements of a list without the last one
+initt [] = error "You can't do that with the empty list!"
+initt (h:[]) = []
+initt (h:t) = h : initt t
+
+
+-- 32 Take the first n elements from a list
+takee, dropp :: Int -> [a] -> [a]
+takee _ [] = []
+takee 0 _ = []
+takee number (h:t) = h : takee (number-1) t
+
+-- 33. Drop the first n elements from a list
+dropp _ [] = []
+dropp 0 arr = arr
+dropp number (h:t) = dropp (number - 1) t
+
+-- 34. Zipping lists
+zipp :: [a] -> [b] -> [(a, b)]
+zipp [] [] = []
+zipp arr [] = []
+zipp [] arr = []
+zipp (h1:t1) (h2:t2) = (h1, h2) : zipp t1 t2
